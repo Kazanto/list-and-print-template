e4803dcda8ae5670d5dd21fef62ee6c51b401a9c
Gerência Ecommerce Kazanto <felipealmeidacorrea@outlook.com.br>
2022-05-19 17:40:13 -0400

    first commit

---
+++ js/kzt-base-slick-config.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('.header').slick({
+        waitForAnimate: false,
+        autoplay: !0,
+        autoplaySpeed: 4500,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: false,
+        zIndex: 0, 
+    });
+  });
